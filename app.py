b179d7acf6c403307d4ba5c2a2a531fcc7241018
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2020-08-12 21:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,45 +12,21 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-  
-  
-@app.route('/')
-def home():
-    return render_template('aboutus.html')
-  
-  
-@app.route('/')
-def home():
-    return render_template('testyourself.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        radius_mean = float(request.form['Enter the Radius'])
-        texture_mean = float(request.form['Enter the Texture'])
-        area_mean = float(request.form['Enter the Area'])
-        concavity_mean = float(request.form['Enter the concavity'])
-        concave_points_mean = float(request.form['Enter the Concave points'])
-        radius_se = float(request.form['Enter the Radius'])
-        area_se = float(request.form['Enter the Area'])
-        radius_worst = float(request.form['Enter the Radius'])
-        texture_worst = float(request.form['Enter the Texture'])
-        perimeter_worst = float(request.form['Enter the Parameters'])
-        area_worst = float(request.form['Enter the Area'])
-        smoothness_worst = float(request.form['Enter the Smoothness'])
-        compactness_worst = float(request.form['Enter the Compactness'])
-        concave_points_worst = float(request.form['Enter the Concave points'])
+        features = [float(x) for x in request.form.values()]
+        final_features = [np.array(features)]
+        my_prediction = classifier.predict(final_features)
 
-        input_data = np.array([[radius_mean, texture_mean, area_mean, concavity_mean, concave_points_mean, radius_se, area_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concave points_worst]])
-
-        my_prediction = classifier.predict(input_data)
-
-        if prediction[0][0] == 1:
+        if my_prediction[0] == 1:
             out = 'Oops, Danger you have to take care of yourself. You are Malignant'
-        elif prediction[0][0] == 0:
+        elif my_prediction[0] == 0:
             out = 'Great, you need not to worry. You are Benign'
-        return render_template('testyourself.html', prediction_text='{}'.format(out))
+
+
+        return render_template('index.html', prediction_text='{}'.format(out))
 
 if __name__ == '__main__':
     app.run(debug=True)
